775afe4d0ea57d75d38aeb427f20c3ec82bad930
Heistrack <110886871+Heistrack@users.noreply.github.com>
2023-03-06 19:49:58 +0100

    fix(Fluere): Fixed exceptions handler from BudgetController

--- src/main/java/com/example/final_project/api/controllers/BudgetController.java
+++ src/main/java/com/example/final_project/api/controllers/BudgetController.java
@@ -95,15 +95,4 @@ public class BudgetController {
                 .map(BudgetResponseDto::fromDomain));
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
     }
-}

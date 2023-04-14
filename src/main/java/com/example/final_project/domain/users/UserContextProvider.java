b68535b511d65a1e538fec83ba05362cb3f09574
Szymon <ximonq@gmail.com>
2023-04-14 17:23:00 +0200

    fix(fluere): fixed user context holder, added UserContextProvider class

---
+++ src/main/java/com/example/final_project/domain/users/UserContextProvider.java
@@ -0,0 +1,11 @@
+package com.example.final_project.domain.users;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public enum UserContextProvider {
+    INSTANCE;
+
+    public static FluereAppUser getUserContext() {
+        return (FluereAppUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+}
